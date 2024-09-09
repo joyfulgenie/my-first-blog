db44c3eb3e5683e968ee2e7519daad6793dd7c94
김병진 <b.j.kim@kakao.com>
2024-09-09 10:35:53 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def post_list(request): #blog.urls.py 에서 이 함수를 특정 url 과 연결한다. path('', views.post_list, name='post_list'),
+    objs = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':objs})   #'posts':objs  ->  템플릿테그:변수
+    # 템플릿을 연결하고 텝플릿 내부의 변수를 정의한다.
