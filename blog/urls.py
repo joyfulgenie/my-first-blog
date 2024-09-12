56730bcfc9aa5108d115241403118f08f198d374
김병진 <b.j.kim@kakao.com>
2024-09-12 17:20:56 +0900

    'final'

--- blog/urls.py
+++ blog/urls.py
@@ -16,4 +16,10 @@ urlpatterns = [
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     # 삭제
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    # comment 달기
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name = 'add_comment_to_post'),
+    # 코멘트 허가
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    # 코멘트 삭제
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
